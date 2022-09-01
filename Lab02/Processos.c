68f32cbf35bba16708f857fa4b0783e74c4e3cba
Elieder Damasceno Sousa <eliedersousa@gmail.com>
2022-08-31 23:34:50 -0300

    Modificação completa do programa. :)

--- Lab02/Processos.c
+++ Lab02/Processos.c
@@ -1,17 +1,63 @@
-#include <math.h>
+#include <windows.h>
 #include <stdio.h>
+#include <math.h>
 
-double Fatorial( int n ) {
-	return (sqrt(2 * 3.1415 * n) * pow( n / 2.718281828459045235360, n ));
+unsigned long long int result = 1;
+
+struct Args {
+  int init;
+  int end;
+};
+
+DWORD WINAPI thread( void* arg ) {
+
+    HANDLE  hStdout = NULL;
+    struct Args* teste = arg;
+    
+    if( (hStdout = GetStdHandle(STD_OUTPUT_HANDLE)) == INVALID_HANDLE_VALUE ) return 1;
+    
+    int w;
+
+    for( w = teste->init; w < teste->end+1; w++ ) {
+      result *= w;
+    }
+    
+    return 0; 
 }
+ 
+void main(int argc, char** argv) {
+    int resultado1 = 1;
+    int resultado2 = 1;
+    struct Args um;
+    struct Args dois;
+    long NUM = strtol(argv[1], NULL, 10);
+    um.init = 1;
+    um.end = floor(NUM/2);
+    dois.init = floor(NUM/2)+1;
+    dois.end = NUM;
 
+    HANDLE Handle_Of_Thread_1 = 0;
+    HANDLE Handle_Of_Thread_2 = 0;
+    HANDLE Array_Of_Thread_Handles[2];
 
-int main() {
-	int num;
-	printf("Digite o numero para calcular o fatorial: ");
-	scanf("%d", &num);
-	
-	printf("%f", Fatorial(num));
-  
-  return 0;
+    Handle_Of_Thread_1 = CreateThread( NULL, 0, thread, &um, 0, NULL);  
+    if ( Handle_Of_Thread_1 == NULL) {
+      ExitProcess(resultado1);
+    }
+        
+    Handle_Of_Thread_2 = CreateThread( NULL, 0, thread, &dois, 0, NULL);  
+    if ( Handle_Of_Thread_2 == NULL)
+        ExitProcess(resultado2);
+
+    Array_Of_Thread_Handles[0] = Handle_Of_Thread_1;
+    Array_Of_Thread_Handles[1] = Handle_Of_Thread_2;
+    
+    // Wait until all threads have terminated.
+    WaitForMultipleObjects( 2, Array_Of_Thread_Handles, TRUE, INFINITE);
+
+    // Close all thread handles upon completion.
+    CloseHandle(Handle_Of_Thread_1);
+    CloseHandle(Handle_Of_Thread_2);
+
+    printf("%llu", result);
 }
